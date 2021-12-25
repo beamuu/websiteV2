f10282784bf1c1984917257c028cd4d2e22073be
nutchanonc <nutchanonc@github.com>
2021-12-25 13:43:04 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Navbar';
+import Contact from './views/contact';
+import Me from './views/Me';
+import Projects from './views/projects';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar></Navbar>
+            <Me />
+            <Projects />
+            <Contact />
+            
+        </>
+    );
 }
 
 export default App;
